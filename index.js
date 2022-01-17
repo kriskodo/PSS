ed25b66c3c822b533d22fb4332153aabcd065972
HDElemental <kristiangeorgiev99ss@abv.bg>
2022-01-17 15:44:42 +0200

    Add null coalescing to package ref and delivery request

--- index.js
+++ index.js
@@ -843,10 +843,14 @@ window.Pontica = {
 								console.log(items);
 								const chatRefs = JSON.parse(items.chatRefs);
 								let counter = items.colorCounter;
-								let packageReference = document.querySelectorAll(".o__admin-note")[0].innerHTML.split('Reference: ')[1].split(' ')[0].split('<br>');
-								let deliveryRequest = document.querySelectorAll(".o__admin-note")[0].innerHTML.split('Delivery Request: ')[1].split(' ')[0].split('<br>');
-								packageReference = packageReference.length > 0 ? packageReference[0] : null;
-								deliveryRequest = deliveryRequest.length > 0 ? deliveryRequest[0] : null;
+								let packageReference = document.querySelectorAll(".o__admin-note")[0].innerHTML.split('Reference: ')[1]?.split(' ')[0]?.split('<br>');
+								let deliveryRequest = document.querySelectorAll(".o__admin-note")[0].innerHTML.split('Delivery Request: ')[1]?.split(' ')[0]?.split('<br>');
+								
+								if(!packageReference && !deliveryRequest) return;
+								
+								packageReference = packageReference?.length > 0 ? packageReference[0] : null;
+								deliveryRequest = deliveryRequest?.length > 0 ? deliveryRequest[0] : null;
+								
 								const openedChatInfo = document.getElementsByClassName('ember-view conversation__stream')[0];
 								const openedChatInfoMessages = openedChatInfo.getElementsByTagName('p');
 								const currentChatId = window.location.href.match(/conversations\/(\d+)/)[1];
