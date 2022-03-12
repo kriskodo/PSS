97ff0b0d61b7111c779adcd03ce58eabc727226a
Kristian G. Georgiev <Kristian.Georgiev@ponticasolutions.com>
2022-03-12 15:14:57 +0200

    Remove test

--- dist/main.js
+++ dist/main.js
@@ -4512,10 +4512,6 @@ var scriptsInformation = [{
   title: "Magnify Fountain Images + Rotation",
   description: "Hover Download button to see image, use R to rotate",
   tags: [DEPARTMENTS.ADMIN]
-}, {
-  title: "test",
-  description: "test",
-  tags: [DEPARTMENTS.CHATS]
 }];
 window.Pontica = {
   scriptsInformation: scriptsInformation,
--- src/index.js
+++ src/index.js
@@ -43,12 +43,7 @@ const scriptsInformation = [
 			title: "Magnify Fountain Images + Rotation",
 			description: "Hover Download button to see image, use R to rotate",
 			tags: [DEPARTMENTS.ADMIN]
-		},
-		{
-			title: "test",
-			description: "test",
-			tags: [DEPARTMENTS.CHATS]
-		},
+		}
 ];
 
 window.Pontica = {
